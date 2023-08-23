83e0fb922440841f58c8613894f699d70a435db1
Rioba Ian <riobafelix@gmail.com>
2023-08-23 09:31:25 +0300

    migrate user to add more fields in registration

--- priv/repo/migrations/20230823062552_add_fields_to_users.exs
+++ priv/repo/migrations/20230823062552_add_fields_to_users.exs
@@ -2,6 +2,10 @@ defmodule Sendmail.Repo.Migrations.AddFieldsToUsers do
   use Ecto.Migration
 
   def change do
-
+    alter table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :msisdn, :string
+    end
   end
 end
